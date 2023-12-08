6824191867149fb21004d10fa09c5bb9ed75e5af
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-08 02:06:01 -0300

    05-Showing employees
    05-Showing employees

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,29 @@
+import axios from 'axios'
+import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const employees = await axios.get('http://localhost:3004/employees')
+  const showEmployees = employees.data.map(employee =>(
+    <div key={employee.id} className='col'>
+       <div className='card'>
+          <div className='card-body'>
+            <h3 className='card-title'>{employee.fullname}</h3>
+            <hr/>
+            <p className='card-text'>{employee.position}</p>
+            <p className='card-text'>{employee.age}</p>
+            <Link href={`/employees/${employee.id}`} className='btn btn-primary'>Go to empl</Link>
+          </div>
+       </div>
+    </div>
+  ))
+
+
   return (
     <>
-      <h1>Employees:</h1>  
+      <h1>Employees:</h1>
+      <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+        {showEmployees}
+      </div>
     </>
   )
 }
