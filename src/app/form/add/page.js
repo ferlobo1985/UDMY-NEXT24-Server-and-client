946eb0c5a8e7377f99018a93057f28206fa64ce6
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-08 03:42:27 -0300

    11-UseFormState
    11-UseFormState

--- src/app/form/add/page.js
+++ src/app/form/add/page.js
@@ -1,27 +1,26 @@
+'use client'
 import {addEmployee} from '@/helpers/actions'
+import { useFormState } from 'react-dom'
 
 export default function AddPage() {
-
-  // async function onSubmit(formdata){
-  //   'use server'
-  //   await axios.post(`http://localhost:3004/employees`,{
-  //     fullname: formdata.get('fullname'),
-  //     position: formdata.get('position'),
-  //     age: formdata.get('age'),
-  //   });
-  //   redirect('/');
-  // }
-
+  const initialState = {message:null}
+  const [state, formAction] = useFormState(addEmployee,initialState)
 
     return (
       <>
         <h1>Add Employee</h1>
-        <form action={addEmployee}>
+        <form action={formAction}>
           <input type="text" className="form-control mb-3" id="fullname" placeholder="Enter the fullname" name="fullname"/>
 
           <input type="text" className="form-control mb-3" id="position" placeholder="Enter the position" name="position"/>
 
           <input type="text" className="form-control mb-3" id="age" placeholder="Enter the age" name="age"/>
+
+          {state.message ?
+            <div className='alert alert-danger' role='alert'>
+              {state.message}
+            </div>
+          :null}
 
           <button type="submit" className="btn btn-primary">
             Add employee
--- src/helpers/actions.js
+++ src/helpers/actions.js
@@ -4,13 +4,18 @@ import axios from 'axios';
 import { redirect } from 'next/navigation';
 
 
-export async function addEmployee(formdata){
-    await axios.post(`http://localhost:3004/employees`,{
-        fullname: formdata.get('fullname'),
-        position: formdata.get('position'),
-        age: formdata.get('age'),
-    });
-    redirect('/');
+export async function addEmployee(formState,formdata){
+    return {
+        message:'Something is wrong'
+    }
+
+
+    // await axios.post(`http://localhost:3004/employees`,{
+    //     fullname: formdata.get('fullname'),
+    //     position: formdata.get('position'),
+    //     age: formdata.get('age'),
+    // });
+    // redirect('/');
 }
 
 
