2102102537dc2adbf84dee9e0807df71459490b9
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-08 16:42:57 -0300

    13-Loading UI
    13-Loading UI

--- src/app/employees/[id]/page.js
+++ src/app/employees/[id]/page.js
@@ -1,5 +1,7 @@
 
-export default function EmployeeByIDPage() {
+export default async function EmployeeByIDPage() {
+    await new Promise((resolve)=> setTimeout(resolve,3000))
+
     return (
       <>
         <h1>Employee ID</h1>  
--- src/components/counter.js
+++ src/components/counter.js
@@ -5,7 +5,7 @@ import { counterTrigger } from '@/helpers/actions'
 
 export default function Counter(){
     const [count,setCount] = useState(0);
-    console.log('where')
+   // console.log('where')
 
     return(
         <div>
