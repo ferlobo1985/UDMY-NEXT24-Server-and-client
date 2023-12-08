e2492bc7fb5fa10e1cefc61d6ee05c47cd515c2b
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-08 16:48:53 -0300

    13-Employee by ID
    13-Employee by ID

--- src/app/employees/[id]/page.js
+++ src/app/employees/[id]/page.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 
-export default async function EmployeeByIDPage() {
-    await new Promise((resolve)=> setTimeout(resolve,3000))
+export default async function EmployeeByIDPage(props) {
+  const pageID = props.params.id;
+  const employee = await axios.get(`http://localhost:3004/employees/${pageID}`);
 
     return (
       <>
-        <h1>Employee ID</h1>  
+        <h1>{employee.data.fullname}</h1>
+        <div>
+          <h4>Position:{employee.data.position}</h4>
+          <h4>Age:{employee.data.age}</h4>
+        </div>
       </>
     )
   }
