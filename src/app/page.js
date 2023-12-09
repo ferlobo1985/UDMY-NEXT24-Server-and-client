0bd789158f1201f84c77692f133c75624bfd6e87
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-09 03:32:12 -0300

    24-Header and cookies
    24-Header and cookies

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Counter from '@/components/counter'; 
+import { headers, cookies } from 'next/headers'
 
 // export const dynamic = 'force-dynamic';
 // export const revalidate = 5;
@@ -16,6 +17,20 @@ async function getEmployees(){
 
 
 export default async function Home() {
+  // const authHeader = headers().get('auth');
+  // const authHeader = headers().entries()
+  // const authHeader = headers().forEach()
+  // const authHeader = headers().has('Content-Type');
+  // const authHeader = headers().keys()
+  // const authHeader = headers().values()
+
+  // const cookiesStore = cookies().get('someCookie')
+  // const cookiesStore = cookies().getAll().map((cookie)=>{})
+  // const cookiesStore = cookies().has('whatevername')
+
+  // cookies().set('name','value',{secure:true});
+  // cookies().delete('name')
+
   const employees = await getEmployees()
   const showEmployees = employees.map(employee =>(
     <div key={employee.id} className='col'>
