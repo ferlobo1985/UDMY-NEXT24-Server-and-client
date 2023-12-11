a4156695e1204cacd86235288ec2418b50807b4a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-11 16:41:28 -0300

    03-Route Handlers
    03-Route Handlers

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,2 +1,21 @@
+import { cookies, headers  } from 'next/headers'
 
 
+export async function GET(request){
+    // const x = request.nextUrl.searchParams.get('x');
+    // const y = request.nextUrl.searchParams.get('y');
+    const cookieStore = cookies();
+    const token = cookieStore.get('token');
+
+    const headersList = headers();
+    console.log(headersList.get('Authorization'))
+
+    const res = await fetch('http://localhost:3004/employees',{
+        next:{revalidate:30}
+    });
+    const employees = await res.json();
+    return Response.json(employees,{
+        status:200,
+        headers:{'Set-Cookie':'token=Bearer kjhzbkajdbckdajhcbakdcb'}
+    })
+}
