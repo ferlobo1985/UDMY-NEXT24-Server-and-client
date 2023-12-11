1325a6181edd07e0d8078014e71ca21d09647362
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-11 16:49:41 -0300

    04-Route Handlers posting
    04-Route Handlers posting

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,5 +1,25 @@
 import { cookies, headers  } from 'next/headers'
 
+export async function POST(request){
+    const data = await request.json();
+    const res = await fetch('http://localhost:3004/employees',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if(!res.ok){
+        return Response.json(res.statusText,{
+            status: res.status
+        })
+    }
+
+    return Response.json('ok',{status:200})
+}
+
+
 
 export async function GET(request){
     // const x = request.nextUrl.searchParams.get('x');
